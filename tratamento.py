83401c896dafe24a7430e96f9f502c69b330b22b
ThalesAugusto <thalllesaugustos28@gmail.com>
2021-08-06 11:35:14 -0300

    main

---
+++ tratamento.py
@@ -0,0 +1,163 @@
+import pandas as pd
+import os
+# from module import tratamento
+
+empresas = ["ABEV3", "AZUL4", "BTOW3", "B3SA3", "BBSE3", "BRML3", "BBDC4", "BRAP4", "BBAS3", "BRKM5", "BRFS3", "BPAC11", "CRFB3", "CCRO3", "CMIG4", "HGTX3", "CIEL3", "COGN3", "CPLE6", "CSAN3", "CPFE3", "CVCB3", "CYRE3", "ECOR3", "ELET6", "EMBR3", "ENBR3", "ENGI11", "ENEV3", "EGIE3", "EQTL3", "EZTC3", "FLRY3", "GGBR4", "GOAU4", "GOLL4", "NTCO3", "HAPV3", "HYPE3", "IGTA3", "GNDI3", "ITSA4", "ITUB4", "JBSS3", "JHSF3", "KLBN11", "RENT3", "LCAM3", "LAME4", "LREN3", "MGLU3", "MRFG3", "BEEF3", "MRVE3", "MULT3", "PCAR3", "PETR4", "BRDT3", "PRIO3", "QUAL3", "RADL3", "RAIL3", "SBSP3", "SANB11", "CSNA3", "SULA11", "SUZB3", "TAEE11", "VIVT3", "TIMS3", "TOTS3", "UGPA3", "USIM5", "VALE3", "VVAR3", "WEGE3", "YDUQ3"]
+
+fundamentos = {}
+arquivos = os.listdir("balancos")
+for arquivo in arquivos:
+    nome = arquivo[-9:-4]
+    if "11" in nome:
+        nome = arquivo[-10:-4]
+    if nome in empresas:
+        print(nome)
+        balanco = pd.read_excel(f'balancos/{arquivo}', sheet_name=0)
+        balanco.iloc[0, 0] = nome
+        balanco.columns = balanco.iloc[0]
+        balanco = balanco[1:]
+        balanco = balanco.set_index(nome)
+        dre = pd.read_excel(f'balancos/{arquivo}', sheet_name=1)
+        dre.iloc[0, 0] = nome
+        dre.columns = dre.iloc[0]
+        dre = dre[1:]
+        dre = dre.set_index(nome)
+        fundamentos[nome] = balanco.append(dre)
+
+cotacoes_df = pd.read_excel("Cotacoes.xlsx")
+cotacoes = {}
+for empresa in cotacoes_df["Empresa"].unique():
+    cotacoes[empresa] = cotacoes_df.loc[cotacoes_df['Empresa']==empresa, :]
+
+for empresa in empresas:
+    if cotacoes[empresa].isnull().values.any():
+        cotacoes.pop(empresa)
+        fundamentos.pop(empresa)
+empresas = list(cotacoes.keys())
+
+for empresa in fundamentos:
+    tabela = fundamentos[empresa].T
+    tabela.index = pd.to_datetime(tabela.index, format="%d/%m/%Y")
+    tabela_cotacao = cotacoes[empresa].set_index("Date")
+    tabela_cotacao = tabela_cotacao[["Adj Close"]]
+    
+    tabela = tabela.merge(tabela_cotacao, right_index=True, left_index=True)
+    tabela.index.name = empresa
+    fundamentos[empresa] = tabela
+
+colunas = list(fundamentos["ABEV3"].columns)
+
+for empresa in empresas:
+    if set(colunas) != set(fundamentos[empresa].columns):
+        fundamentos.pop(empresa)
+
+texto_colunas = ";".join(colunas)
+
+colunas_modificadas = []
+for coluna in colunas:
+    if colunas.count(coluna) == 2 and coluna not in colunas_modificadas:
+        texto_colunas = texto_colunas.replace(";" + coluna + ";",";" + coluna + "_1;", 1)
+        colunas_modificadas.append(coluna)
+colunas = texto_colunas.split(';')
+
+for empresa in fundamentos:
+    fundamentos[empresa].columns = colunas
+
+valores_vazios = dict.fromkeys(colunas, 0)
+total_linhas = 0
+for empresa in fundamentos:
+    tabela = fundamentos[empresa]
+    total_linhas += tabela.shape[0]
+    for coluna in colunas:
+        qtde_vazios = pd.isnull(tabela[coluna]).sum()
+        valores_vazios[coluna] += qtde_vazios
+
+remover_colunas = []
+for coluna in valores_vazios:
+    if valores_vazios[coluna] > 50:
+        remover_colunas.append(coluna)
+
+for empresa in fundamentos:
+    fundamentos[empresa] = fundamentos[empresa].drop(remover_colunas, axis=1)
+    fundamentos[empresa] = fundamentos[empresa].ffill()
+
+fundamentos["ABEV3"].shape
+
+data_inicial = "12/20/2021"
+data_final = "04/20/2021"
+
+from pandas_datareader import data as web
+df_ibov = web.DataReader('^BVSP', data_source='yahoo', start=data_inicial, end=data_final)
+
+import numpy as np
+
+datas = fundamentos["ABEV3"].index
+for data in datas:
+    if data not in df_ibov.index:
+        df_ibov,loc[data] = np.nan
+df_ibov = df_ibov.sort_index()
+df_ibov = df_ibov.ffill()
+df_ibov = df_ibov.rename(columns={"Adj Close": "IBOV"})
+for empresa in fundamentos:
+    fundamentos[empresa] = fundamentos[empresa].merge(df_ibov[["IBOV"]], left_index=True, right_index=True)
+
+for empresa in fundamentos:
+    fundamento = fundamentos[empresa]
+    fundamento = fundamentos.sort_index()
+    for coluna in fundamento:
+        if "Adj Close" in coluna or "IBOV" in coluna:
+            pass 
+        else:
+            condicoes = [
+                (fundamento[coluna].shift(1) > 0) & (fundamento[coluna] < 0),
+                (fundamento[coluna].shift(1) < 0) & (fundamento[coluna] > 0),
+                (fundamento[coluna].shift(1) < 0) & (fundamento[coluna] < 0),
+                (fundamento[coluna].shift(1) == 0) & (fundamento[coluna] > 0),
+                (fundamento[coluna].shift(1) == 0) & (fundamento[coluna] < 0),
+                (fundamento[coluna].shift(1) < 0) & (fundamento[coluna] == 0),
+            ]
+            valores = [
+                -1,
+                1,
+                (abs(fundamento[coluna].shift(1)) - abs(fundamento[coluna])) / abs(fundamento[coluna].shift(1)),
+                1,
+                -1,
+                1,
+            ]
+            fundamento[coluna] = np.select(condicoes, valores, default=fundamento[coluna] / fundamento[coluna].shift(1) - 1)
+    
+    fundamento["Adj Close"] = fundamento["Adj Close"].shift(-1) / fundamento["Adj Close"] -1
+    fundamento["IBOV"] = fundamento["IBOV"].shift(-1) / fundamento["IBOV"] -1
+    fundamento["Resultado"] = fundamento["Adj Close"] - fundamento["IBOV"]
+    condicoes = [
+        (fundamento["Resultado"] > 0),
+        (fundamento["Resultado"] < 0) & (fundamento["Resultado"] >= -0.02),
+        (fundamento["Resultado"] < -0.2)
+    ]
+    valores = [2, 1, 0]
+    fundamento["Decisao"] = np.select(condicoes, valores)
+    fundamentos[empresa] = fundamento
+
+colunas = list(fundamentos["ABEV3"].columns)
+valores_vazios = dict.fromkeys(colunas, 0)
+total_linhas = 0
+for empresa in fundamentos:
+    tabela = fundamentos[empresa]
+    total_linhas += tabela.shape[0]
+    for coluna in colunas:
+        qtde_vazios = pd.isnull(tabela[coluna]).sum()
+        valores_vazios[coluna] += qtde_vazios
+
+remover_colunas = []
+for coluna in valores_vazios:
+    if valores_vazios[coluna] > (total_linhas / 3):
+        remover_colunas.append(coluna)
+
+for empresa in fundamentos:
+    fundamentos[empresa] = fundamentos[empresa].drop(remover_colunas, axis=1)
+    fundamentos[empresa] = fundamentos[empresa].fillna(0)
+
+for empresa in fundamentos:
+    fundamentos[empresa] = fundamentos[empresa].drop(["Adj Close", "IBOV", "Resultado"], axis=1)
+
+copia_fundamentos = fundamentos.copy()
