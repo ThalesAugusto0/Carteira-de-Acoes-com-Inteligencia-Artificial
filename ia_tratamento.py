4bd7d28226b088b31ce88359aeac87f3460fa599
ThalesAugusto <thalllesaugustos28@gmail.com>
2021-08-18 17:17:39 -0300

    main

--- ia_tratamento.py
+++ ia_tratamento.py
@@ -2,22 +2,126 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sys
-from tratamento import copia_fundamentos
 from IPython.display import display
-from tratamento import base_dados
-from tratamento import copia_fundamentos
+from tratamento import *
 
-sys.path.insert(0, r"C:\Users\thall\Documents\Projetos\Carteira-de-Acoes-com-Inteligencia-Artificial\tratamento.py")
+data_inicial = "12/20/2012" 
+data_final = "04/20/2021"
 
+from pandas_datareader import data as web
+df_ibov = web.DataReader('^BVSP', data_source='yahoo', start=data_inicial, end=data_final)
+
+import numpy as np
+
+datas = fundamentos["ABEV3"].index
+for data in datas:
+    if data not in df_ibov.index:
+        df_ibov.loc[data] = np.nan
+df_ibov = df_ibov.sort_index()
+df_ibov = df_ibov.ffill()
+df_ibov = df_ibov.rename(columns={"Adj Close": "IBOV"})
+for empresa in fundamentos:
+    fundamentos[empresa] = fundamentos[empresa].merge(df_ibov[["IBOV"]], left_index=True, right_index=True)
+display(fundamentos["ABEV3"])
+
+# tornar os nossos indicadores em percentuais
+# fundamento%tri = fundamento tr / fundamento tri anterior
+# cotacao%tri = cotacao tri seguinte / cotacao tri
+for empresa in fundamentos:
+    fundamento = fundamentos[empresa]
+    fundamento = fundamento.sort_index()
+    for coluna in fundamento:
+        if "Adj Close" in coluna or "IBOV" in coluna:
+            pass
+        else:
+            # pegar a cotação anterior
+            condicoes = [
+                (fundamento[coluna].shift(1) > 0) & (fundamento[coluna] < 0),
+                (fundamento[coluna].shift(1) < 0) & (fundamento[coluna] > 0),
+                (fundamento[coluna].shift(1) < 0) & (fundamento[coluna] < 0),
+                (fundamento[coluna].shift(1) == 0) & (fundamento[coluna] > 0),
+                (fundamento[coluna].shift(1) == 0) & (fundamento[coluna] < 0),
+                (fundamento[coluna].shift(1) < 0) & (fundamento[coluna] == 0),
+            ]
+            valores = [
+                -1,
+                1,
+                (abs(fundamento[coluna].shift(1)) - abs(fundamento[coluna])) / abs(fundamento[coluna].shift(1)),
+                1,
+                -1,
+                1,
+            ]
+            fundamento[coluna] = np.select(condicoes, valores, default=fundamento[coluna] / fundamento[coluna].shift(1) - 1)
+    # pegar cotação seguinte
+    fundamento["Adj Close"] = fundamento["Adj Close"].shift(-1) / fundamento["Adj Close"] - 1
+    fundamento["IBOV"] = fundamento["IBOV"].shift(-1) / fundamento["IBOV"] - 1
+    fundamento["Resultado"] = fundamento["Adj Close"] - fundamento["IBOV"]
+    condicoes = [
+        (fundamento["Resultado"] > 0),
+        (fundamento["Resultado"] < 0) & (fundamento["Resultado"] >= -0.02),
+        (fundamento["Resultado"] < -0.02)
+    ]
+    valores = [2, 1, 0]
+    fundamento["Decisao"] = np.select(condicoes, valores)
+    
+    fundamentos[empresa] = fundamento
+display(fundamentos["ABEV3"])
+
+# remover valores vazios
+colunas = list(fundamentos["ABEV3"].columns)
+valores_vazios = dict.fromkeys(colunas, 0)
+total_linhas = 0
+for empresa in fundamentos:
+    tabela = fundamentos[empresa]
+    total_linhas += tabela.shape[0]
+    for coluna in colunas:
+        qtde_vazios = pd.isnull(tabela[coluna]).sum()
+        valores_vazios[coluna] += qtde_vazios
+print(valores_vazios)
+print(total_linhas)
+
+remover_colunas = []
+for coluna in valores_vazios:
+    if valores_vazios[coluna] > (total_linhas / 3):
+        remover_colunas.append(coluna)
+
+for empresa in fundamentos:
+    fundamentos[empresa] = fundamentos[empresa].drop(remover_colunas, axis=1)
+    fundamentos[empresa] = fundamentos[empresa].fillna(0)
+
+for empresa in fundamentos:
+    fundamentos[empresa] = fundamentos[empresa].drop(["Adj Close", "IBOV", "Resultado"], axis=1)
+print(fundamentos["ABEV3"].shape)
+
+copia_fundamentos = fundamentos.copy()
+
+base_dados = pd.DataFrame()
+for empresa in copia_fundamentos:
+    copia_fundamentos[empresa] = copia_fundamentos[empresa][1:-1]
+    copia_fundamentos[empresa] = copia_fundamentos[empresa].reset_index(drop=True)
+    base_dados = base_dados.append(copia_fundamentos[empresa])
+display(base_dados)
+
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+display(base_dados['Decisao'].value_counts(normalize=True).map("{:.1%}".format))
+fig = px.histogram(base_dados, x="Decisao", color="Decisao")
+fig.show()
+
+# vou tirar a categoria 1 e transformar em 0
 base_dados.loc[base_dados["Decisao"]==1, "Decisao"] = 0
 display(base_dados['Decisao'].value_counts(normalize=True).map("{:.1%}".format))
 fig = px.histogram(base_dados, x="Decisao", color="Decisao")
 fig.show()
 
 correlacoes = base_dados.corr()
+
 fig, ax = plt.subplots(figsize=(15, 10))
 sns.heatmap(correlacoes, cmap="Wistia", ax=ax)
+plt.show()
+display(correlacoes)
 
 correlacoes_encontradas = []
 for coluna in correlacoes:
@@ -25,12 +129,15 @@ for coluna in correlacoes:
         if linha != coluna:
             valor = abs(correlacoes.loc[linha, coluna])
             if valor > 0.8 and (coluna, linha, valor) not in correlacoes_encontradas:
-                correlacoes_encontradas.append(linha, coluna, valor)
+                correlacoes_encontradas.append((linha, coluna, valor))
                 print(f"Correlação Encontrada: {linha} e {coluna}. Valor: {valor}")
 
 remover = ['Ativo Circulante', 'Contas a Receber_1', 'Tributos a Recuperar', 'Passivo Total', 'Passivo Circulante', 'Patrimônio Líquido', 'Capital Social Realizado', 'Receita Líquida de Vendas e/ou Serviços', 'Resultado Bruto', 'Despesas Gerais e Administrativas']
 base_dados = base_dados.drop(remover, axis=1)
 
+print(base_dados.shape)
+
+# vamos treinar uma arvore de decisao e pegar as caracteristicas mais importantes dela
 from sklearn.ensemble import ExtraTreesClassifier
 
 modelo = ExtraTreesClassifier(random_state=1)
@@ -38,21 +145,26 @@ x = base_dados.drop("Decisao", axis=1)
 y = base_dados["Decisao"]
 modelo.fit(x, y)
 
-carecterisricas_importantes = pd.DataFrame(modelo.feature_importances_, x.columns).sort_values(by=0, ascending=False)
-display(carecterisricas_importantes)
-top10 = list(carecterisricas_importantes.index)[:10]
+caracteristicas_importantes = pd.DataFrame(modelo.feature_importances_, x.columns).sort_values(by=0, ascending=False)
+display(caracteristicas_importantes)
+top10 = list(caracteristicas_importantes.index)[:10]
+print(top10)
 
 from sklearn.preprocessing import StandardScaler
 
 def ajustar_scaler(tabela_original):
     scaler = StandardScaler()
     tabela_auxiliar = tabela_original.drop("Decisao", axis=1)
+    
     tabela_auxiliar = pd.DataFrame(scaler.fit_transform(tabela_auxiliar), tabela_auxiliar.index, tabela_auxiliar.columns)
+    tabela_auxiliar["Decisao"] = tabela_original["Decisao"]
     return tabela_auxiliar
 
 nova_base_dados = ajustar_scaler(base_dados)
-top10.append("Decisao")
+top10.append("Decisao") 
+
 nova_base_dados = nova_base_dados[top10].reset_index(drop=True)
+display(nova_base_dados)
 
 from sklearn.model_selection import train_test_split
 
@@ -64,7 +176,7 @@ x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, random_state=1)
 from sklearn.dummy import DummyClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
-dummy = DummyClassifier(strategy="stratified", randon_state=2)
+dummy = DummyClassifier(strategy="stratified", random_state=2)
 dummy.fit(x_treino, y_treino)
 previsao_dummy = dummy.predict(x_teste)
 
@@ -76,8 +188,9 @@ def avaliar(y_teste, previsoes, nome_modelo):
     sns.heatmap(cf_matrix, annot=True, cmap="Blues", fmt=',')
     plt.show()
     print("#" * 50)
-
-avaliar(y_teste, previsoes_dummy, "Dummy")
+    
+    
+avaliar(y_teste, previsao_dummy, "Dummy")
 
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -110,7 +223,7 @@ for nome_modelo in modelos:
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import make_scorer, precision_score
 
-modelo_final = modelo["RandomForest"]
+modelo_final = modelos["RandomForest"]
 
 n_estimators = range(10, 251, 30)
 max_features = list(range(2, 11, 2))
@@ -120,29 +233,33 @@ min_samples_split = range(2, 11, 2)
 precision2_score = make_scorer(precision_score, labels=[2], average='macro')
 
 grid = GridSearchCV(
-    estimator=RandomForestClassifier(),
-    param_grid={
-        'n_estimators': n_estimators,
-        'max_features': max_features,
-        'min_samples_split': min_samples_split,
-        'random_state': [1],
-    },
+        estimator=RandomForestClassifier(),
+        param_grid={
+            'n_estimators': n_estimators,
+            'max_features': max_features,
+            'min_samples_split': min_samples_split,
+            'random_state': [1],
+        },
+        scoring=precision2_score,
 )
 
 resultado_grid = grid.fit(x_treino, y_treino)
+print("Ajuste feito")
 
 modelo_tunado = resultado_grid.best_estimator_
 previsoes = modelo_tunado.predict(x_teste)
 avaliar(y_teste, previsoes, "RandomForest Tunado")
 
 ult_tri_fundamentos = fundamentos.copy()
-ult_tri_fundamentos = pd.DataFrame()
+ult_tri_base_dados = pd.DataFrame()
 lista_empresas = []
 for empresa in ult_tri_fundamentos:
     ult_tri_fundamentos[empresa] = ult_tri_fundamentos[empresa][-1:]
     ult_tri_fundamentos[empresa] = ult_tri_fundamentos[empresa].reset_index(drop=True)
     ult_tri_base_dados = ult_tri_base_dados.append(ult_tri_fundamentos[empresa])
     lista_empresas.append(empresa)
+display(ult_tri_base_dados)
+print(lista_empresas)
 
 ult_tri_base_dados = ult_tri_base_dados.reset_index(drop=True)
 ult_tri_base_dados = ult_tri_base_dados[top10]
@@ -151,19 +268,22 @@ ult_tri_base_dados = ult_tri_base_dados.drop("Decisao", axis=1)
 display(ult_tri_base_dados)
 
 previsoes_ult_tri = modelo_tunado.predict(ult_tri_base_dados)
+print(previsoes_ult_tri)
 
 carteira = []
 carteira_inicial = []
 
 for i, empresa in enumerate(lista_empresas):
     if previsoes_ult_tri[i] == 2:
+        print(empresa)
         carteira_inicial.append(1000)
-        cotacao = cotacao[empresa]
+        cotacao = cotacoes[empresa]
         cotacao = cotacao.set_index("Date")
         cotacao_inicial = cotacao.loc["2020-12-31", "Adj Close"]
         cotacao_final = cotacao.loc["2021-03-31", "Adj Close"]
-        percentual = cotacao_inicial / cotacao_final
+        percentual = cotacao_final / cotacao_inicial
         carteira.append(1000 * percentual)
+        
 saldo_inicial = sum(carteira_inicial)
 saldo_final = sum(carteira)
 print(saldo_inicial, saldo_final)
--- tratamento.py
+++ tratamento.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import os
+from IPython.display import display
 
 empresas = ["ABEV3", "AZUL4", "BTOW3", "B3SA3", "BBSE3", "BRML3", "BBDC4", "BRAP4", "BBAS3", "BRKM5", "BRFS3", "BPAC11", "CRFB3", "CCRO3", "CMIG4", "HGTX3", "CIEL3", "COGN3", "CPLE6", "CSAN3", "CPFE3", "CVCB3", "CYRE3", "ECOR3", "ELET6", "EMBR3", "ENBR3", "ENGI11", "ENEV3", "EGIE3", "EQTL3", "EZTC3", "FLRY3", "GGBR4", "GOAU4", "GOLL4", "NTCO3", "HAPV3", "HYPE3", "IGTA3", "GNDI3", "ITSA4", "ITUB4", "JBSS3", "JHSF3", "KLBN11", "RENT3", "LCAM3", "LAME4", "LREN3", "MGLU3", "MRFG3", "BEEF3", "MRVE3", "MULT3", "PCAR3", "PETR4", "BRDT3", "PRIO3", "QUAL3", "RADL3", "RAIL3", "SBSP3", "SANB11", "CSNA3", "SULA11", "SUZB3", "TAEE11", "VIVT3", "TIMS3", "TOTS3", "UGPA3", "USIM5", "VALE3", "VVAR3", "WEGE3", "YDUQ3"]
-
+# fundamentos = {
+#     "ABEV3": balanco_dre_abev3,
+#     "MGLU3": balanco_dre_mglu3
+# }
 fundamentos = {}
 arquivos = os.listdir("balancos")
 for arquivo in arquivos:
@@ -11,15 +15,22 @@ for arquivo in arquivos:
         nome = arquivo[-10:-4]
     if nome in empresas:
         print(nome)
+        # pegar o balanco daquela empresa
         balanco = pd.read_excel(f'balancos/{arquivo}', sheet_name=0)
+        # na primeira coluna colocar o título com o nome da empresa
         balanco.iloc[0, 0] = nome
+        # pegar 1ª linha e tornar um cabeçalho
         balanco.columns = balanco.iloc[0]
         balanco = balanco[1:]
+        # tornar a 1ª coluna (que agora tem o nome da emrpesa)
         balanco = balanco.set_index(nome)
         dre = pd.read_excel(f'balancos/{arquivo}', sheet_name=1)
+        # na primeira coluna colocar o título com o nome da empresa
         dre.iloc[0, 0] = nome
+        # pegar 1ª linha e tornar um cabeçalho
         dre.columns = dre.iloc[0]
         dre = dre[1:]
+        # tornar a 1ª coluna (que agora tem o nome da emrpesa)
         dre = dre.set_index(nome)
         fundamentos[nome] = balanco.append(dre)
 
@@ -28,12 +39,20 @@ cotacoes = {}
 for empresa in cotacoes_df["Empresa"].unique():
     cotacoes[empresa] = cotacoes_df.loc[cotacoes_df['Empresa']==empresa, :]
 
+print(len(cotacoes))
+
 for empresa in empresas:
     if cotacoes[empresa].isnull().values.any():
         cotacoes.pop(empresa)
         fundamentos.pop(empresa)
 empresas = list(cotacoes.keys())
+print(len(empresas))
 
+# no cotacoes: jogar as datas para índice
+# no fundamnetos:
+    # trocar linhas por colunas
+    # tratar as datas para formato de data do python
+    # juntar os fundamentos com a coluna Adj Close das cotacoes
 for empresa in fundamentos:
     tabela = fundamentos[empresa].T
     tabela.index = pd.to_datetime(tabela.index, format="%d/%m/%Y")
@@ -43,12 +62,14 @@ for empresa in fundamentos:
     tabela = tabela.merge(tabela_cotacao, right_index=True, left_index=True)
     tabela.index.name = empresa
     fundamentos[empresa] = tabela
+display(fundamentos["ABEV3"])
 
 colunas = list(fundamentos["ABEV3"].columns)
 
 for empresa in empresas:
     if set(colunas) != set(fundamentos[empresa].columns):
         fundamentos.pop(empresa)
+print(len(fundamentos))
 
 texto_colunas = ";".join(colunas)
 
@@ -58,10 +79,16 @@ for coluna in colunas:
         texto_colunas = texto_colunas.replace(";" + coluna + ";",";" + coluna + "_1;", 1)
         colunas_modificadas.append(coluna)
 colunas = texto_colunas.split(';')
+print(colunas)
 
+# implementar as colunas nas tabelas
 for empresa in fundamentos:
     fundamentos[empresa].columns = colunas
 
+# valores_vazios = {
+#     "Ativo Total": 0,
+#     "Passivo Total": 0,
+# }
 valores_vazios = dict.fromkeys(colunas, 0)
 total_linhas = 0
 for empresa in fundamentos:
@@ -70,6 +97,8 @@ for empresa in fundamentos:
     for coluna in colunas:
         qtde_vazios = pd.isnull(tabela[coluna]).sum()
         valores_vazios[coluna] += qtde_vazios
+print(valores_vazios)
+print(total_linhas)
 
 remover_colunas = []
 for coluna in valores_vazios:
@@ -81,93 +110,3 @@ for empresa in fundamentos:
     fundamentos[empresa] = fundamentos[empresa].ffill()
 
 fundamentos["ABEV3"].shape
-
-data_inicial = "2021-04-20"  ##mudança na forma de tratar as datas yyyy/mm/dd
-data_final = "2021-12-20"
-
-from pandas_datareader import data as pdr
-import yfinance as yfin
-
-yfin.pdr_override()
-df_ibov = pdr.get_data_yahoo(f'^BVSP', start=data_inicial, end=data_final)
-print(df_ibov)
-
-import numpy as np
-
-datas = fundamentos["ABEV3"].index
-for data in datas:
-    if data not in df_ibov.index:
-        df_ibov.loc[data] = np.nan
-df_ibov = df_ibov.sort_index()
-df_ibov = df_ibov.ffill()
-df_ibov = df_ibov.rename(columns={"Adj Close": "IBOV"})
-for empresa in fundamentos:
-    fundamentos[empresa] = fundamentos[empresa].merge(df_ibov[["IBOV"]], left_index=True, right_index=True)
-
-for empresa in fundamentos:
-    fundamento = fundamentos[empresa]
-    fundamento = fundamento.sort_index()
-    for coluna in fundamento:
-        if "Adj Close" in coluna or "IBOV" in coluna:
-            pass
-        else:
-            condicoes = [
-                (fundamento[coluna].shift(1) > 0) & (fundamento[coluna] < 0),
-                (fundamento[coluna].shift(1) < 0) & (fundamento[coluna] > 0),
-                (fundamento[coluna].shift(1) < 0) & (fundamento[coluna] < 0),
-                (fundamento[coluna].shift(1) == 0) & (fundamento[coluna] > 0),
-                (fundamento[coluna].shift(1) == 0) & (fundamento[coluna] < 0),
-                (fundamento[coluna].shift(1) < 0) & (fundamento[coluna] == 0),
-            ]
-            valores = [
-                -1,
-                1,
-                (abs(fundamento[coluna].shift(1)) - abs(fundamento[coluna])) / abs(fundamento[coluna].shift(1)),
-                1,
-                -1,
-                1,
-            ]
-            fundamento[coluna] = np.select(condicoes, valores, default=fundamento[coluna] / fundamento[coluna].shift(1) - 1)
-
-    fundamento["Adj Close"] = fundamento["Adj Close"].shift(-1) / fundamento["Adj Close"] - 1
-    fundamento["IBOV"] = fundamento["IBOV"].shift(-1) / fundamento["IBOV"] - 1
-    fundamento["Resultado"] = fundamento["Adj Close"] - fundamento["IBOV"]
-    condicoes = [
-        (fundamento["Resultado"] > 0),
-        (fundamento["Resultado"] < 0) & (fundamento["Resultado"] >= -0.02),
-        (fundamento["Resultado"] < -0.02)
-    ]
-    valores = [2, 1, 0]
-    fundamento["Decisao"] = np.select(condicoes, valores)
-    
-    fundamentos[empresa] = fundamento
-
-colunas = list(fundamentos["ABEV3"].columns)
-valores_vazios = dict.fromkeys(colunas, 0)
-total_linhas = 0
-for empresa in fundamentos:
-    tabela = fundamentos[empresa]
-    total_linhas += tabela.shape[0]
-    for coluna in colunas:
-        qtde_vazios = pd.isnull(tabela[coluna]).sum()
-        valores_vazios[coluna] += qtde_vazios
-
-remover_colunas = []
-for coluna in valores_vazios:
-    if valores_vazios[coluna] > (total_linhas / 3):
-        remover_colunas.append(coluna)
-
-for empresa in fundamentos:
-    fundamentos[empresa] = fundamentos[empresa].drop(remover_colunas, axis=1)
-    fundamentos[empresa] = fundamentos[empresa].fillna(0)
-
-for empresa in fundamentos:
-    fundamentos[empresa] = fundamentos[empresa].drop(["Adj Close", "IBOV", "Resultado"], axis=1)
-
-copia_fundamentos = fundamentos.copy()
-
-base_dados = pd.DataFrame()
-for empresa in copia_fundamentos:
-    copia_fundamentos[empresa] = copia_fundamentos[empresa][1:-1]
-    copia_fundamentos[empresa] = copia_fundamentos[empresa].reset_index(drop=True)
-    base_dados = base_dados.append(copia_fundamentos[empresa])
