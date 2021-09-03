4565ac12cfaa8cb48be29d1b399222555656b11c
ThalesAugusto <thalllesaugustos28@gmail.com>
2021-09-03 18:16:34 -0300

    main

---
+++ ia.py
@@ -0,0 +1,100 @@
+from ia_tratamento import *
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+
+modelos = {
+    "AdaBoost": AdaBoostClassifier(random_state=1),
+    "DecisionTree": DecisionTreeClassifier(random_state=1),
+    "RandomForest": RandomForestClassifier(random_state=1),
+    "ExtraTree": ExtraTreesClassifier(random_state=1),
+    "GradientBoost": GradientBoostingClassifier(random_state=1),
+    "KNN": KNeighborsClassifier(),
+    "LogisticRegression": LogisticRegression(random_state=1),
+    "NaiveBayes": GaussianNB(),
+    "SVM": SVC(random_state=1),
+    "RedeNeural": MLPClassifier(random_state=1, max_iter=400),
+}
+
+for nome_modelo in modelos:
+    modelo = modelos[nome_modelo]
+    modelo.fit(x_treino, y_treino)
+    previsoes = modelo.predict(x_teste)
+    avaliar(y_teste, previsoes, nome_modelo)
+    modelos[nome_modelo] = modelo
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer, precision_score
+
+modelo_final = modelos["RandomForest"]
+
+n_estimators = range(10, 251, 30)
+max_features = list(range(2, 11, 2))
+max_features.append('auto')
+min_samples_split = range(2, 11, 2)
+
+precision2_score = make_scorer(precision_score, labels=[2], average='macro')
+
+grid = GridSearchCV(
+        estimator=RandomForestClassifier(),
+        param_grid={
+            'n_estimators': n_estimators,
+            'max_features': max_features,
+            'min_samples_split': min_samples_split,
+            'random_state': [1],
+        },
+        scoring=precision2_score,
+)
+
+resultado_grid = grid.fit(x_treino, y_treino)
+print("Ajuste feito")
+
+modelo_tunado = resultado_grid.best_estimator_
+previsoes = modelo_tunado.predict(x_teste)
+avaliar(y_teste, previsoes, "RandomForest Tunado")
+
+ult_tri_fundamentos = fundamentos.copy()
+ult_tri_base_dados = pd.DataFrame()
+lista_empresas = []
+for empresa in ult_tri_fundamentos:
+    ult_tri_fundamentos[empresa] = ult_tri_fundamentos[empresa][-1:]
+    ult_tri_fundamentos[empresa] = ult_tri_fundamentos[empresa].reset_index(drop=True)
+    ult_tri_base_dados = ult_tri_base_dados.append(ult_tri_fundamentos[empresa])
+    lista_empresas.append(empresa)
+display(ult_tri_base_dados)
+print(lista_empresas)
+
+ult_tri_base_dados = ult_tri_base_dados.reset_index(drop=True)
+ult_tri_base_dados = ult_tri_base_dados[top10]
+ult_tri_base_dados = ajustar_scaler(ult_tri_base_dados)
+ult_tri_base_dados = ult_tri_base_dados.drop("Decisao", axis=1)
+display(ult_tri_base_dados)
+
+previsoes_ult_tri = modelo_tunado.predict(ult_tri_base_dados)
+print(previsoes_ult_tri)
+
+carteira = []
+carteira_inicial = []
+
+for i, empresa in enumerate(lista_empresas):
+    if previsoes_ult_tri[i] == 2:
+        print(empresa)
+        carteira_inicial.append(1000)
+        cotacao = cotacoes[empresa]
+        cotacao = cotacao.set_index("Date")
+        cotacao_inicial = cotacao.loc["2020-12-31", "Adj Close"]
+        cotacao_final = cotacao.loc["2021-03-31", "Adj Close"]
+        percentual = cotacao_final / cotacao_inicial
+        carteira.append(1000 * percentual)
+        
+saldo_inicial = sum(carteira_inicial)
+saldo_final = sum(carteira)
+print(saldo_inicial, saldo_final)
+print(saldo_final / saldo_inicial)
+
+variacao_ibov = df_ibov.loc["2021-03-31", "IBOV"] / df_ibov.loc["2020-12-31", "IBOV"]
+print(variacao_ibov)
