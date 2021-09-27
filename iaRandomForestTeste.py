f285ac72e72daa4054151df5c08e0be75f73a487
Thales Augusto <thalllesaugustos28@gmail.com>
2021-09-27 09:52:18 -0300

    main

---
+++ iaRandomForestTeste.py
@@ -0,0 +1,92 @@
+from ia import *
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+
+
+modelo = RandomForestClassifier(n_estimators = 1000,random_state=0) #! n_estimators(numero de arvores) quanto maior melhor(consome mais do pc) 
+#! min_samples_leaf numero de exemplos minimo em um nó muda-lo pode melhorar ou piorar 
+
+
+modelo.fit(x_treino, y_treino)
+previsoes = modelo.predict(x_teste)
+avaliar(y_teste, previsoes, nome_modelo)
+
+
+#? daqui para baixo continua igual 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer, precision_score
+
+
+modelo_final = modelo
+
+n_estimators = range(10, 251, 30)
+max_features = list(range(2, 11, 2))
+max_features.append('auto')
+min_samples_split = range(2, 11, 2)
+
+precision2_score = make_scorer(precision_score, labels=[2], average='macro')
+
+grid = GridSearchCV(
+        estimator=RandomForestClassifier(),
+        param_grid={
+            'n_estimators': n_estimators,
+            'max_features': max_features,
+            'min_samples_split': min_samples_split,
+            'random_state': [1],
+        },
+        scoring=precision2_score,
+)
+
+resultado_grid = grid.fit(x_treino, y_treino)
+print("Ajuste feito")
+
+modelo_tunado = resultado_grid.best_estimator_
+previsoes = modelo_tunado.predict(x_teste)
+avaliar(y_teste, previsoes, "RandomForest Tunado")
+
+ult_tri_fundamentos = fundamentos.copy()
+ult_tri_base_dados = pd.DataFrame()
+lista_empresas = []
+for empresa in ult_tri_fundamentos:
+    ult_tri_fundamentos[empresa] = ult_tri_fundamentos[empresa][-1:]
+    ult_tri_fundamentos[empresa] = ult_tri_fundamentos[empresa].reset_index(drop=True)
+    ult_tri_base_dados = ult_tri_base_dados.append(ult_tri_fundamentos[empresa])
+    lista_empresas.append(empresa)
+display(ult_tri_base_dados)
+print(lista_empresas)
+
+ult_tri_base_dados = ult_tri_base_dados.reset_index(drop=True)
+ult_tri_base_dados = ult_tri_base_dados[top10]
+ult_tri_base_dados = ajustar_scaler(ult_tri_base_dados)
+ult_tri_base_dados = ult_tri_base_dados.drop("Decisao", axis=1)
+display(ult_tri_base_dados)
+
+previsoes_ult_tri = modelo_tunado.predict(ult_tri_base_dados)
+print(previsoes_ult_tri)
+
+carteira = []
+carteira_inicial = []
+
+for i, empresa in enumerate(lista_empresas):
+    if previsoes_ult_tri[i] == 2:
+        print(empresa)
+        carteira_inicial.append(1000)
+        cotacao = cotacoes[empresa]
+        cotacao = cotacao.set_index("Date")
+        cotacao_inicial = cotacao.loc["2020-12-31", "Adj Close"]
+        cotacao_final = cotacao.loc["2021-03-31", "Adj Close"]
+        percentual = cotacao_final / cotacao_inicial
+        carteira.append(1000 * percentual)
+        
+saldo_inicial = sum(carteira_inicial)
+saldo_final = sum(carteira)
+print(saldo_inicial, saldo_final)
+print(saldo_final / saldo_inicial)
+
+variacao_ibov = df_ibov.loc["2021-03-31", "IBOV"] / df_ibov.loc["2020-12-31", "IBOV"]
+print(variacao_ibov)
